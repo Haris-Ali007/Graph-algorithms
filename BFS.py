d5f45484d3ec9aea51c1b4fd369c9e61203c3e51
Haris Ali <haris.ali@formulatrix.com>
2023-05-03 22:37:37 +0500

    Create BFS class

--- BFS.py
+++ BFS.py
@@ -1,6 +1,35 @@
 from Graphs import Graph
 from queue import Queue
 
+class BFS():
+
+    def __init__(self, graph):
+        self.graph = graph
+        
+    def bfs(self):
+        q = Queue()
+        visited = []
+        parents = []
+        seen = set([graph.nodes[self.start_node]])            
+        q.put(graph.nodes[self.start_node])
+
+        while not q.empty():
+            node = q.get()    
+            data = node.data
+            neighbours = node.neighbours
+            if data not in visited:
+                visited.append(data)            
+                for each_node in neighbours:
+                    if each_node not in seen:
+                        seen.add(each_node)
+                        q.put(each_node)
+
+    def shortest_path(self, start_node, end_node):
+        self.start_node = start_node
+        self.end_node = end_node
+        self.bfs()
+
+
 
 if __name__=="__main__":
     graph = Graph()
@@ -18,20 +47,5 @@ if __name__=="__main__":
     graph.add_node(4, 3)    
     graph.print_graph()
 
-    q = Queue()
-    start_node = 0
-    end_node = 5
-    visited = []
-    prev = []
-            
-    q.put(graph.nodes[start_node])
-   
-    while not q.empty():
-        node = q.get()    
-        data = node.data
-        neighbours = node.neighbours
-        if data not in visited:
-            visited.append(data)            
-            for each_node in neighbours:
-                q.put(each_node)
-            
+    
+    
